237683a0243a0e3334967a22001cca033f4657c3
Dean Ralph <54666004+scootiemagee@users.noreply.github.com>
2020-01-06 14:07:18 +0000

    Update pythonHeader.py
    updated to use f strings

--- pythonHeader.py
+++ pythonHeader.py
@@ -10,7 +10,7 @@ def pythonTitle(title):
   for _ in range(1, varSpacing):
     varDashes += "-"
 
-  varReturn = varDashes + "\n"
+  varReturn = f"{varDashes}\n"
 
   if len(title) % 2 != 0:
     varHalf = ((varSpacing - len(title)) / 2) - 1
@@ -22,7 +22,7 @@ def pythonTitle(title):
     varReturn += f"\n{varDashes}" 
 
   varReturn += "\n# Dean Ralph"
-  varReturn += "\n# " + str(datetime.date.today())
+  varReturn += f"\n# {str(datetime.date.today())}"
   varReturn += "\n#"
   varReturn += f"\n# Project Title: {title}"
   varReturn += f"\n# Short Description:"
